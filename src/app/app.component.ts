ff77d17854387f9ae59b3a33b906f81ded86d2df
suraj.nikhade <suraj.nikhade@coditas.com>
2021-08-23 15:28:43 +0530

    first committ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'August20';
+  number1 = 50;
+  number2 = 25;
+
+  first_name = 'Suraj';
+  last_name = 'Nikhade';
+
+  interpolationCall() {
+    return 'Your are Inside a Function';
+  }
 }
